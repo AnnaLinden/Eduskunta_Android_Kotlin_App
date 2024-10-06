76ef8b50821df99484d5db20e5ea7d22ec0873c4
AnnaLinden <linden.a.anna@gmail.com>
2024-10-06 13:02:38 +0300

    MPDao

---
+++ app/src/main/java/com/example/eduskunta_feedback_app/data/MPDao.kt
@@ -0,0 +1,21 @@
+package com.example.eduskunta_feedback_app.data
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.eduskunta_feedback_app.data.model.MP
+import kotlinx.coroutines.flow.Flow
+
+interface MPDao {
+    @Query("SELECT * FROM mp_table")
+    fun getAllMPs(): Flow<List<MP>>
+
+    @Query("SELECT * FROM mp_table WHERE party = :party")
+    fun getMPsByParty(party: String): Flow<List<MP>>
+
+    @Query("SELECT * FROM mp_table WHERE hetekaId = :hetekaId")
+    fun getMPById(hetekaId: Int): Flow<MP>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(mps: List<MP>)
+}
