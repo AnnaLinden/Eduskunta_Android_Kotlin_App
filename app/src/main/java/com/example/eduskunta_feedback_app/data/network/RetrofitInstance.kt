d383b1fcef95c1093ddc684ec896fabff3fda98f
AnnaLinden <linden.a.anna@gmail.com>
2024-10-06 12:58:03 +0300

    RetrofitInstance

--- app/src/main/java/com/example/eduskunta_feedback_app/data/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/eduskunta_feedback_app/data/network/RetrofitInstance.kt
@@ -1,4 +1,16 @@
 package com.example.eduskunta_feedback_app.data.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
+    private const val BASE_URL = "https://users.metropolia.fi/"
+
+    val api: MPApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MPApiService::class.java)
+    }
 }
