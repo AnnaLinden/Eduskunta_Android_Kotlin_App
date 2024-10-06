55f4111d582492a59302985f1be0ed17b4c9b97e
AnnaLinden <linden.a.anna@gmail.com>
2024-10-06 11:31:53 +0300

    mp data class

---
+++ app/src/main/java/com/example/eduskunta_feedback_app/data/model/MP.kt
@@ -0,0 +1,16 @@
+package com.example.eduskunta_feedback_app.data.model
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "mp_table")
+data class MP (
+    @PrimaryKey val hetekaId: Int,
+    val seatNumber: Int?,
+    val lastname: String?,
+    val firstname: String?,
+    val party: String?,
+    val minister: Boolean?,
+    val pictureUrl: String?
+)
